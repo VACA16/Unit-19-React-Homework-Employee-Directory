acfbb46176c762b38bde1a8079a3d2a3ae99b3ca
Pastor <randyvaca11@gmail.com>
2020-06-20 12:42:33 -0500

    added index.js for colum, row, and added home.js to pages

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Body() {
-  return <div />;
+export default function Body(props) {
+  return <div className={`body${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
